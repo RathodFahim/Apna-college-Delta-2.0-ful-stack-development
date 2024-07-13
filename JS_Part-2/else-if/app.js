e041711421b4504b2bc7ef0fd478ce692dcade80
RathodFahim <rathodrathod75850@gmail.com>
2024-07-13 15:44:53 +0530

    conditional statement

---
+++ JS_Part-2/else-if/app.js
@@ -0,0 +1,11 @@
+let marks = 65;
+
+if (marks > 90) {
+    console.log('Amazing');
+} else if (marks > 70) {
+    console.log('Good');
+} else if (marks > 60) {
+    console.log('Average');
+} else {
+    console.log('Poor');
+}
---
+++ JS_Part-2/if_else/app.js
@@ -0,0 +1,5 @@
+let firstName = "Fahim";
+
+if (firstName === "Fahim") {
+    console.log(`Hello ${firstName}, How are you?`);
+}
