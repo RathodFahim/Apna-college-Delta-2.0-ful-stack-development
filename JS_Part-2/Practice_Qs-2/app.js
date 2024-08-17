a9691b4d1c798e689dec35b5aafb3ce98bdcf579
RathodFahim <rathodrathod75850@gmail.com>
2024-08-17 14:52:03 +0530

    Practice Qs-2

---
+++ JS_Part-2/Practice_Qs-2/app.js
@@ -0,0 +1,11 @@
+let siZe = "L";
+
+if(siZe === "XL") {
+    console.log("Price is 250");
+} else if(siZe === "L") {
+    console.log("Price is 200");
+} else if(siZe === "M") {
+    console.log("Price is 100");
+} else {
+    console.log("Price is 50");
+}
